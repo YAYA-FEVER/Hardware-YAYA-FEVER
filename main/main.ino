3c0ccdc2b7071ce4ca7fbf32606c35f4c9f569a4
n-tk11 <tulsorawit@hotmail.com>
2022-02-17 17:26:23 +0700

    fix main

--- main/main.ino
+++ main/main.ino
@@ -2,37 +2,30 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-void getMode(); //request for Mode and min humidity every 5 sec
-void getExist(); //request if plant exist in the shelf every 5 sec
-void postStat(int,int,int); //post humidity(soil,air), temp to admid every hour
-void postHeight(int); //post height every week
-void Wifi_Connect();
+//void getMode(); //request for Mode and min humidity every 5 sec
+//void getExist(); //request if plant exist in the shelf every 5 sec
+//void postStat(int,int,int); //post humidity(soil,air), temp to admid every hour
+//void postHeight(int); //post height every week
+//void Wifi_Connect();
 
-const char* ssid = "";
-const char* password = "";
 
-const plant_id = 1;
+const char* ssid = "Tong";
+const char* password = "23456789";
+
+const int plant_id = 1;
 bool autoMode = false; //false = Manual , true = auto
 bool isActive = false; //Is plant exist in the shelf.
 int minHumid;
 
-StaticJsonDocument<JSON_OBJECT_SIZE(2)> JsonMode;
+
+
+StaticJsonDocument<2*JSON_OBJECT_SIZE(3)> JsonMode;
 StaticJsonDocument<JSON_OBJECT_SIZE(1)> JsonExist;
 StaticJsonDocument<JSON_OBJECT_SIZE(4)> JsonStat;
-StaticJsonDocument<JSON_OBJECT_SIZE(2)> JsonHeight
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+StaticJsonDocument<JSON_OBJECT_SIZE(2)> JsonHeight;
 
 
-void WiFi_Connect(){
+void Wifi_Connect(){
   WiFi.disconnect();
   WiFi.begin(ssid,password);
   while(WiFi.status()!=WL_CONNECTED){
@@ -48,7 +41,8 @@ void WiFi_Connect(){
 
 void getMode(){
    if(WiFi.status() == WL_CONNECTED){
-      http.begin(...); // No get url yet!!!!
+      HTTPClient http;
+      http.begin("https://ecourse.cpe.ku.ac.th/exceed05/api/hardware/auto_mode/1");
       int httpCode = http.GET();
       if(httpCode == HTTP_CODE_OK){
         String payload = http.getString();
@@ -60,12 +54,29 @@ void getMode(){
           Serial.println(httpCode);
           minHumid=JsonMode["humidity_soil_front"];
           autoMode=JsonMode["activate_auto"];
+          Serial.println(minHumid);
+          Serial.println(autoMode);
         }
       }else{
         Serial.println(httpCode);
         Serial.println("ERROR on HTTP Request");
       }
    }else{
-    WiFi_Connect();
+    Wifi_Connect();
    }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Wifi_Connect();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  getMode();
+  // put your main code here, to run repeatedly:
+
 }
